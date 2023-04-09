394a3a15592bb5449ef46233b23d04ffa88570f2
lequangduccode <88152285+lequangduccode@users.noreply.github.com>
2023-04-09 23:41:49 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,6 +39,21 @@
         return false;
     });
 
+
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
+
+
     // Project and Testimonial carousel
     $(".project-carousel, .testimonial-carousel").owlCarousel({
         autoplay: true,
@@ -67,8 +82,6 @@
             }
         }
     });
-
-    
     
 })(jQuery);
 
